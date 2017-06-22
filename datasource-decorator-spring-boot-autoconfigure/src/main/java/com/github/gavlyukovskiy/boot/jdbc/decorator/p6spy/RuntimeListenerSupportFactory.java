b9e8cddda25147627642d0a5a06fad53956ebdaa
Arthur Gavlyukovskiy <agavlyukovskiy@gmail.com>
2017-06-22 22:22:09 +0300

    added missing license, merged RuntimeListenerHolder and RuntimeListenerFactory

--- datasource-decorator-spring-boot-autoconfigure/src/main/java/com/github/gavlyukovskiy/boot/jdbc/decorator/p6spy/RuntimeListenerSupportFactory.java
+++ datasource-decorator-spring-boot-autoconfigure/src/main/java/com/github/gavlyukovskiy/boot/jdbc/decorator/p6spy/RuntimeListenerSupportFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.gavlyukovskiy.boot.jdbc.decorator.p6spy;
 
 import com.p6spy.engine.event.CompoundJdbcEventListener;
@@ -8,6 +24,7 @@ import com.p6spy.engine.spy.option.P6OptionsRepository;
 import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Factory to support all defined {@link JdbcEventListener} in the context.
@@ -17,12 +34,17 @@ import java.util.List;
  */
 public class RuntimeListenerSupportFactory implements P6Factory {
 
+    private static List<JdbcEventListener> listeners;
+
     private final CompoundJdbcEventListener listener;
 
     public RuntimeListenerSupportFactory() {
-        List<JdbcEventListener> listeners = RuntimeListenerHolder.getListeners();
         Assert.state(listeners != null, "This factory should not been initialized if there are no listeners available");
         listener = new CompoundJdbcEventListener(listeners);
+    }
+
+    static void setListeners(List<JdbcEventListener> listeners) {
+        RuntimeListenerSupportFactory.listeners = Objects.requireNonNull(listeners, "listeners should not be null");
     }
 
     @Override
