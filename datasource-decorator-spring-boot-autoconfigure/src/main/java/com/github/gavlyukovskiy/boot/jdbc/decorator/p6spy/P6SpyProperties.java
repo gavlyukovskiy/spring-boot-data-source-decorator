15dce240efc1e19e80fbe4b2b66daad2f3749bda
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2025-03-30 11:59:20 +0200

    Cleanup tracing related properties removed in 1.9.0

--- datasource-decorator-spring-boot-autoconfigure/src/main/java/com/github/gavlyukovskiy/boot/jdbc/decorator/p6spy/P6SpyProperties.java
+++ datasource-decorator-spring-boot-autoconfigure/src/main/java/com/github/gavlyukovskiy/boot/jdbc/decorator/p6spy/P6SpyProperties.java
@@ -16,8 +16,6 @@
 
 package com.github.gavlyukovskiy.boot.jdbc.decorator.p6spy;
 
-import org.springframework.boot.context.properties.DeprecatedConfigurationProperty;
-
 import java.util.regex.Pattern;
 
 /**
@@ -51,11 +49,6 @@ public class P6SpyProperties {
     private String logFormat;
 
     /**
-     * Tracing related properties
-     */
-    private P6SpyTracing tracing = new P6SpyTracing();
-
-    /**
      * Class file to use (only with logging=custom).
      * The class must implement {@link com.p6spy.engine.spy.appender.FormattedLogger}
      */
@@ -86,10 +79,6 @@ public class P6SpyProperties {
         return this.logFormat;
     }
 
-    public P6SpyTracing getTracing() {
-        return this.tracing;
-    }
-
     public String getCustomAppenderClass() {
         return this.customAppenderClass;
     }
@@ -118,10 +107,6 @@ public class P6SpyProperties {
         this.logFormat = logFormat;
     }
 
-    public void setTracing(P6SpyTracing tracing) {
-        this.tracing = tracing;
-    }
-
     public void setCustomAppenderClass(String customAppenderClass) {
         this.customAppenderClass = customAppenderClass;
     }
@@ -135,31 +120,6 @@ public class P6SpyProperties {
         SLF4J,
         FILE,
         CUSTOM
-    }
-
-    @Deprecated
-    public static class P6SpyTracing {
-        /**
-         * Report the effective sql string (with '?' replaced with real values) to tracing systems.
-         * <p>
-         * NOTE this setting does not affect the logging message.
-         */
-        @Deprecated
-        private boolean includeParameterValues = true;
-
-        @Deprecated
-        @DeprecatedConfigurationProperty(
-                reason = "JDBC instrumentation is provided in Spring Cloud Sleuth",
-                replacement = "spring.sleuth.jdbc.p6spy.tracing.include-parameter-values"
-        )
-        public boolean isIncludeParameterValues() {
-            return this.includeParameterValues;
-        }
-
-        @Deprecated
-        public void setIncludeParameterValues(boolean includeParameterValues) {
-            this.includeParameterValues = includeParameterValues;
-        }
     }
 
     public static class P6SpyLogFilter {
